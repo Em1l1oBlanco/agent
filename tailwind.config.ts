95ba589167bd377c3ef20de1ad43084a7eb3688d
Em1l1oBlanco <86232401+Em1l1oBlanco@users.noreply.github.com>
2026-05-28 16:26:56 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,6 @@ export default {
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
   				foreground: 'hsl(var(--card-foreground))'
